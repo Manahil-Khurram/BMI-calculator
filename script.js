a00053c5073dd6dadd62834551569f096839be29
Manahil Khurram <manailshazad44@gmail.com>
2022-10-11 20:14:52 +0500

    only backend

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
+// Gets user input for height and weight in centimeter
+var centimeters = prompt("Enter height in centimeters");
+var pounds = prompt("Enter weight in pounds");
+//changing height in inches and weight in kilos
+var height=centimeters *2.54;
+var weight=pounds*0.45;
+//output on screen for height 
+console.log("height in inches");
+console.log(height+"inches"); 
+//output on screen for weight
+console.log("weight in kilos");
+console.log(weight+"pounds"); 
 
-function Calculate(){
-    var height = document.getElementById("h-input").value;
-    var weight = document.getElementById("w-input").value;
-
+//formula for BMI calculation
     var result = (weight / ((height * height) 
     / 10000)).toFixed(2);
-
+   //If statement for getting output of calculted weight status 
     if(!isNaN(result)){
-        document.getElementById("bmi-output").innerHTML = result;
+        console.log(result);
         if(result < 18.5){
-            document.getElementById("bmi-status").innerHTML = "Underweight";
+            console.log("Underweight");
         }
         else if(result < 25){
-            document.getElementById("bmi-status").innerHTML = "Healthy";
+            console.log("Healthy");
         }
-        else if(result < 30){
-            document.getElementById("bmi-status").innerHTML = "Overweight";
+        else if(result < 39){
+            console.log("Overweight");
         }
-        else{
-            document.getElementById("bmi-status").innerHTML = "Obesity";
+        else if(result>=40){
+            console.log("Obesity");
         }
+        
     }
-}
+    
+
